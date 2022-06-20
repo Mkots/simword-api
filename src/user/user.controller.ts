7716d98f061a077aaf2143021fd69eb8a953d650
Vitalij Komarov <v.komarov@foxford.ru>
2022-06-20 23:33:31 +0300

    Init

---
+++ src/user/user.controller.ts
@@ -0,0 +1,6 @@
+import {FastifyReply, FastifyRequest} from "fastify";
+
+async function createUserHandler(request: FastifyRequest, reply: FastifyReply){
+
+
+}
---
+++ src/user/user.route.ts
@@ -0,0 +1,7 @@
+import {FastifyInstance} from "fastify";
+
+async function userRoute(fastify: FastifyInstance) {
+fastify.post('/')
+}
+
+export default userRoute;
